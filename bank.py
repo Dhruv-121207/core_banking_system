2b1869af80fb9a164a05efe107eb756e6f132c57
Dhruv-121207 <dhruv.121207@gmail.com>
2026-01-13 15:43:39 +0530

    Initial Commit

---
+++ bank.py
@@ -0,0 +1,129 @@
+class BankError(Exception):
+    pass
+
+class AccountNotFoundError(BankError):
+    pass
+
+class InsufficientBalanceError(BankError):
+    pass
+
+class InvalidAmountError(BankError):
+    pass
+
+
+from datetime import datetime
+
+class Account:
+
+    account_counter = 1085
+
+    def __init__(self,acc_holder):
+
+        Account.account_counter += 1
+        self.acc_num = Account.account_counter
+        self.acc_holder = acc_holder
+        self._balance = 0
+        self.tra_history = []
+
+    def deposit(self,amount):
+
+        if amount < 0:
+            raise InvalidAmountError('Amount must be positive')
+        self._balance += amount
+        self.tra_history.append(f'Deposited: ${amount} {datetime.now().strftime("%H:%M:%S")}')
+        return True
+    
+    def withdraw(self,amount):
+
+        if self._balance < amount:
+             raise InsufficientBalanceError('Not enough balance')
+        self._balance -= amount
+        self.tra_history.append(f'Withdrawn: ${amount} {datetime.now().strftime("%H:%M:%S")}')
+        return True
+    
+    def check_balance(self):
+        return f'Balance: {self._balance}'
+    
+    def display_transactions(self):
+        
+        if self.tra_history == []:
+            return False
+        return self.tra_history
+    
+    def __str__(self):
+        return f'Account holder: {self.acc_holder} Account number: {self.acc_num} Balance: {self._balance}'
+    
+class SavingsAccount(Account):
+
+    min_balance = 200
+
+    def __init__(self, acc_holder):
+        super().__init__(acc_holder)
+        self.interest = 1.05
+
+    def apply_interest(self):
+        
+        if self._balance >= SavingsAccount.min_balance:
+            interest_earned = (self._balance * self.interest) - self._balance
+            self._balance *= self.interest
+            self.tra_history.append(f'Interest earned of ${interest_earned}')
+        else:
+            penalty_fee = 10
+            self._balance -= penalty_fee
+            self.tra_history.append(f'Penaly fee of ${penalty_fee} was deducted')
+        
+class CheckingAccount(Account):
+
+    def __init__(self, acc_holder):
+        super().__init__(acc_holder)
+        self.overdraft_limit = 500
+
+    def withdraw(self, amount):
+
+        if amount > self._balance + self.overdraft_limit:
+            raise InsufficientBalanceError('Not enough balance')
+
+        if amount <= self._balance:
+            self._balance -= amount
+            self.tra_history.append(f'Witrhdrawed: ${amount} {datetime.now().strftime("%H:%M:%S")}')
+        else:
+            overdraft_used = amount - self._balance
+            self._balance = 0
+            self.overdraft_limit -= overdraft_used
+            self.tra_history.append(f'Withdrawn using overdraft: ${amount} {datetime.now().strftime("%H:%M:%S")}')
+        return True
+    
+    def display_overdraft(self):
+        return f'overdraft amount: {self.overdraft_limit}'
+    
+class Bank:
+
+    def __init__(self,bank_name):
+        self.name = bank_name
+        self.accounts = {}
+
+    def add_accounts(self,account):
+        self.accounts[account.acc_num] = account
+
+    def get_account(self,acc_num):
+        return self.accounts.get(acc_num)
+    
+    def transfer(self,from_acc_num,to_acc_num,amount):
+        
+        sender = self.get_account(from_acc_num)
+        receiver = self.get_account(to_acc_num)
+
+        if not sender or not receiver:
+            raise AccountNotFoundError('Account not found')
+        
+        if amount > sender._balance:
+            raise InsufficientBalanceError('Insufficient balance for transfer')
+        
+        sender._balance -= amount
+        receiver._balance += amount 
+
+        sender.tra_history.append(f'Transferred ${amount} to {to_acc_num} {datetime.now().strftime("%H:%M:%S")}')
+
+        receiver.tra_history.append(f'Received ${amount} from {from_acc_num} {datetime.now().strftime("%H:%M:%S")}')
+
+        return True
---
+++ main.py
@@ -0,0 +1,134 @@
+from bank import (
+    Bank,
+    SavingsAccount,
+    CheckingAccount,
+    AccountNotFoundError,
+    InsufficientBalanceError,
+    InvalidAmountError
+)
+
+def show_menu():
+    print("\n" + "=" * 30)
+    print("         PYTHON BANK")
+    print("=" * 30)
+    print("1. Create account")
+    print("2. Deposit money")
+    print("3. Withdraw money")
+    print("4. Transfer money")
+    print("5. Check balance")
+    print("6. View transactions")
+    print("7. Check Overdraft Amount")
+    print("8. Exit")
+
+
+def create_account(bank):
+    name = input("Enter account holder name: ").strip()
+
+    print("Select account type")
+    print("1. Savings account")
+    print("2. Checking account")
+
+    choice = input("Enter choice: ").strip()
+
+    if choice == "1":
+        account = SavingsAccount(name)
+    elif choice == "2":
+        account = CheckingAccount(name)
+    else:
+        print("Invalid account type")
+        return
+
+    bank.add_accounts(account)
+    print("Account created successfully")
+    print("Account number:", account.acc_num)
+
+
+def main():
+    bank = Bank("My Bank")
+
+    while True:
+        show_menu()
+        choice = input("Enter option: ").strip()
+
+        try:
+            if choice == "1":
+                create_account(bank)
+
+            elif choice == "2":
+                acc_num = int(input("Enter account number: "))
+                amount = float(input("Enter amount: "))
+
+                account = bank.get_account(acc_num)
+                if not account:
+                    raise AccountNotFoundError("Account not found")
+
+                account.deposit(amount)
+                print("Deposit successful")
+
+            elif choice == "3":
+                acc_num = int(input("Enter account number: "))
+                amount = float(input("Enter amount: "))
+
+                account = bank.get_account(acc_num)
+                if not account:
+                    raise AccountNotFoundError("Account not found")
+
+                account.withdraw(amount)
+                print("Withdrawal successful")
+
+            elif choice == "4":
+                from_acc = int(input("From account number: "))
+                to_acc = int(input("To account number: "))
+                amount = float(input("Enter amount: "))
+
+                bank.transfer(from_acc, to_acc, amount)
+                print("Transfer successful")
+
+            elif choice == "5":
+                acc_num = int(input("Enter account number: "))
+
+                account = bank.get_account(acc_num)
+                if not account:
+                    raise AccountNotFoundError("Account not found")
+
+                print(account.check_balance())
+
+            elif choice == "6":
+                acc_num = int(input("Enter account number: "))
+
+                account = bank.get_account(acc_num)
+                if not account:
+                    raise AccountNotFoundError("Account not found")
+
+                transactions = account.display_transactions()
+                if not transactions:
+                    print("No transactions found")
+                else:
+                    for t in transactions:
+                        print(t)
+            
+            elif choice == "7":
+                acc_num = int(input("Enter account number: "))
+
+                account = bank.get_account(acc_num)
+                if not account:
+                    raise AccountNotFoundError("Account not found")
+                
+                print(account.display_overdraft())
+
+            elif choice == "8":
+                print("Exiting program")
+                break
+
+            else:
+                print("Invalid option")
+
+        except (InvalidAmountError, InsufficientBalanceError, AccountNotFoundError) as e:
+            print("Error:", e)
+
+        except ValueError:
+            print("Invalid input")
+
+
+if __name__ == "__main__":
+    main()
